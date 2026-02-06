f01e24110cf2c511127b982fef223f0a2017333d
igortobias <igortobiaspumbe@gmail.com>
2026-02-06 18:17:00 +0200

    feat: apagar

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import app from "../vercel";
-
-export default async function handler(req: any, res: any) {
-  app(req, res);
-}
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from "./vercel";
-
-const PORT = process.env.PORT || 4003;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
