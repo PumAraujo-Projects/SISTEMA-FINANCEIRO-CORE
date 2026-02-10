f16fcbdb7956e65996c0cfce482b45bee3a01025
igortobias <igortobiaspumbe@gmail.com>
2026-02-10 13:58:54 +0200

    feat: import from right site prisma client

--- src/infrastruture/config/prisma-client.ts
+++ src/infrastruture/config/prisma-client.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '/generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import 'dotenv/config';
+import { PrismaClient } from '../../generated/prisma';
 
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
